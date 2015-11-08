c5da830d80e08be9eeed2ee4761aaf946a509ab4
vs4vijay <vs4vijay@gmail.com>
2015-11-08 14:32:11 +0530

    Add devise token auth

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,30 +1,39 @@
 var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.router', 'templates', 'toaster']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$httpProvider', '$urlRouterProvider', function($stateProvider, $httpProvider, $urlRouterProvider) {
 
-		$urlRouterProvider.otherwise('/cars');
+	$urlRouterProvider.otherwise('/cars');
 
-  	$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: 'app/dashboard/dashboard.html'
-			})
-			.state('dashboard', {
-				url: '/dashboard',
-				controller: 'DashboardCtrl',
-				templateUrl: 'app/dashboard/dashboard.html'
-			})
-			.state('cars', {
-        url: '/cars',
-        controller: 'CarsCtrl',
-        templateUrl: 'app/cars/cars.html',
-        resolve: {
-          CarsList: ['CarService', function(CarService) {
-            return CarService.query();
-          }]
-        }
-      });
-});
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'app/dashboard/dashboard.html'
+		})
+		.state('dashboard', {
+			url: '/dashboard',
+			controller: 'DashboardCtrl',
+			templateUrl: 'app/dashboard/dashboard.html'
+		})
+		.state('cars', {
+      url: '/cars',
+      controller: 'CarsCtrl',
+      templateUrl: 'app/cars/cars.html',
+      resolve: {
+        CarsList: ['CarService', function(CarService) {
+          return CarService.query();
+        }]
+      }
+    });
+
+		$httpProvider.interceptors.push(function() {
+	    return {
+	      responseError: function(res) {
+					console.log('res', res);
+	        return res;
+	      }
+	    };
+	  });
+}]);
 
 
 app.controller('AppCtrl', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
